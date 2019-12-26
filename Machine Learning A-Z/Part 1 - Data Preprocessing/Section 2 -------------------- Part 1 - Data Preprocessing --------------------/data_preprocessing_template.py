6e4f1766bac02346861adefbde3fac38255b59fb
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2019-12-27 03:03:46 +0900

    Modification due to the new anaconda versions

--- Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -11,22 +11,25 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:,1:3])
+from sklearn.impute import SimpleImputer
+missingvalues = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)
+missingvalues = missingvalues.fit(X[:, 1:3])
+X[:, 1:3]=missingvalues.transform(X[:, 1:3])
 
 # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
+# Encoding the Independent Variable
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X), dtype=np.float)
+
+# Encoding Y data
+from sklearn.preprocessing import LabelEncoder
+y = LabelEncoder().fit_transform(y)
+
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
@@ -35,4 +38,4 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
