153cd34c46ad9088920a1a741ad3e88026aa878e
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2020-01-11 14:44:33 +0900

    Modification due to the newer version of libraries

--- Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/missing_data.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/missing_data.py
@@ -11,7 +11,7 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
-from sklearn.impute import SimpleImputer
-missingvalues = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)
-missingvalues = missingvalues.fit(X[:, 1:3])
-X[:, 1:3] = missingvalues.transform(X[:, 1:3])
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
--- Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -20,7 +20,7 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
 
 # Fitting Simple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
