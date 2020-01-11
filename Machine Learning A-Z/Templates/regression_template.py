e85da0d45070cd6866955e4e0bff81f16751b849
Changhyun Cho <59163031+changhyun-cho@users.noreply.github.com>
2020-01-11 15:06:30 +0900

    Update template codes

--- Machine Learning A-Z/Templates/regression_template.py
+++ Machine Learning A-Z/Templates/regression_template.py
@@ -20,13 +20,13 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
 
 # Fitting the Regression Model to the dataset
 # Create your regressor here
 
 # Predicting a new result
-y_pred = regressor.predict([[6.5]])
+y_pred = regressor.predict(6.5)
 
 # Visualising the Regression results
 plt.scatter(X, y, color = 'red')
